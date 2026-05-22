0cda9afe75ab83a87d7264c4e3524441f0d1f072
TemurbekCoder712 <temurbekcoder712@gmail.com>
2026-05-22 15:36:02 +0500

    added

---
+++ Search-in-Rotated-Sorted-Array/Search-in-Rotated-Sorted-Array.js
@@ -0,0 +1,4 @@
+var search = function(nums, target) {
+    return nums.indexOf(target)
+};
+search([4,5,6,7,0,1,2], 0);
---
+++ Search-in-Rotated-Sorted-Array/Search-in-Rotated-Sorted-Array.py
@@ -0,0 +1,4 @@
+def search(nums, target):
+    return nums.index(target)
+
+search([4,5,6,7,0,1,2], 0)
