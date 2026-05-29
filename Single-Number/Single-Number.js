91e57ff35e5960df2b7d79f966fe030da5942496
Temurprogram77 <temurnarzullayev810@gmail.com>
2026-05-29 12:25:49 +0500

    added

---
+++ Single-Number/Single-Number.js
@@ -0,0 +1,8 @@
+var singleNumber = function(nums) {
+    let result = 0;
+    for(let i of nums){
+        result = result ^ i;
+    }
+    return result;
+};
+console.log(singleNumber([4,1,2,1,2]));
